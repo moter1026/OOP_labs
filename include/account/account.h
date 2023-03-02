95c404ce5b4a862666ec87f8e57b382e92033d78
SU_18_611_6 <SU_18_611_6@ssau.ru>
2023-03-02 11:18:11 +0400

    Версия 1.01 Обновил методы создания аккаунтов. и др. мелкие поправки

--- include/account/account.h
+++ include/account/account.h
@@ -1,11 +1,12 @@
 #ifndef FUNCTIONS_HEADER
 #define FUNCTIONS_HEADER
 #define N 255
-#define MAX_COUNT_ACCOUNTS 7
+#define MAX_COUNT_ACCOUNTS 16
 #pragma once
 
 
 namespace user_bank {
+
 	enum class TypeScore {
 		none = 0,
 		calculated,
@@ -15,31 +16,39 @@ namespace user_bank {
 	class Account {
 		friend class User;
 	private:
-		TypeScore type = TypeScore::none;
-		float balance = 0;
-		float percent = 0;
+		TypeScore	type = TypeScore::none;
+		float		balance = 0;
+		float		percent = 0;
 	public:
 		Account();
 		Account(TypeScore type_of_score);
-		void accrual();
-		float getBalance();
-		bool setBalance(float balance);
-		float getPercent();
-		bool setPercent(float percent);
+		void		accrual();
+		float		getBalance();
+		void		setBalance(float balance);
+		float		getPercent();
+		void		setPercent(float percent);
+		Account& operator=(const Account& r);
 	};
 
 	class User {
 	private:
-		char all_name[N * 2];
-		int count_accounts = 0;
+		char		all_name[N * 2];
+		int			count_accounts = 0;
+		Account&	create_calculated_account(unsigned int index, TypeScore type, float balance);
+		Account&	create_dep_or_cre_account(unsigned int index, TypeScore type, float balance, float percent);
+		Account		accounts[MAX_COUNT_ACCOUNTS];
 	public:
-		Account accounts[MAX_COUNT_ACCOUNTS];
 		User();
 		User(const char* name);
-		char* get_all_name();
-		int find_max_balance();
-		bool create_new_account(TypeScore type, float balance);
-		bool create_new_account(TypeScore type, float balance, float percent);
+		char*		get_all_name();
+		int		get_count_accounts();
+		int			find_max_balance();
+		Account&	create_new_account(TypeScore type, float balance);
+		Account&	create_new_account(TypeScore type, float balance, float percent);
+		Account&	create_new_account_in_index(unsigned int index, TypeScore type, float balance);
+		Account&	create_new_account_in_index(unsigned int index, TypeScore type, float balance, float percent);
+		Account&	select_account_with_index(unsigned int index);
+		Account		delete_account_with_index(unsigned int index);
 	};
 };
 #endif
--- src/account.cc
+++ src/account.cc
@@ -5,109 +5,70 @@
 #include <stdexcept>
 #include <iostream>
 #define N 255
-#define MAX_COUNT_ACCOUNTS 7
+//#define MAX_COUNT_ACCOUNTS 7
 
 using namespace user_bank;
 using namespace std;
 
 // Определение работы класса Account
-Account::Account() {
-        balance = 0;
-        TypeScore::calculated;
-}
-Account::Account(TypeScore type_of_score) {
-    try
+Account::Account(): balance(0), type(TypeScore::calculated) {}
+Account::Account(TypeScore type_of_score): balance(0) {
+    switch (type_of_score)
     {
-        switch (type_of_score)
-        {
-        case TypeScore::calculated:
-            balance = 0;
-            type = TypeScore::calculated;
-            break;
-        case TypeScore::deposit:
-            balance = 0;
-            percent = 0;
-            type = TypeScore::deposit;
-            break;
-        case TypeScore::credit:
-            balance = 0;
-            percent = 0;
-            type = TypeScore::credit;
-            break;
-        default:
-            break;
+    case TypeScore::calculated:
+        type = TypeScore::calculated;
+        break;
+    case TypeScore::deposit:
+        percent = 0;
+        type = TypeScore::deposit;
+        break;
+    case TypeScore::credit:
+        percent = 0;
+        type = TypeScore::credit;
+        break;
+    default:
+        break;
+    }
+    if (type != type_of_score)
+        throw runtime_error("Не удалось присвоить тип счёта");
+};
+void        Account::accrual() {
+    switch (type)
+    {
+    case user_bank::TypeScore::calculated:
+        break;
+    case user_bank::TypeScore::deposit:
+        this->balance += this->balance * this->percent / 12;
+        break;
+    case user_bank::TypeScore::credit:
+        if (this->balance < 0) {            //Баланс не отрицательный, поэтому изменений нет
+            this->balance -= fabs(this->balance) * (this->percent) / 12;
         }
-        if (type != type_of_score)
-            throw runtime_error("Не удалось присвоить тип счёта");
-    }
-    catch (const runtime_error& e)
-    {
-        cerr << e.what() << endl;
-    }
+        break;
+    default:
+        throw runtime_error("Тип аккаунта не позволяет совершить действия с его счётом!");
+        break;
+    };
 };
-void Account::accrual() {
-    try
-    {
-        switch (type)
-        {
-        case user_bank::TypeScore::calculated:
-
-            break;
-        case user_bank::TypeScore::deposit:
-            this->balance += this->balance * this->percent / 12;
-            break;
-        case user_bank::TypeScore::credit:
-            if (this->balance < 0) {
-                this->balance -= fabs(this->balance) * (this->percent) / 12;
-            }
-            else
-                cout << "Баланс не отрицательный, поэтому изменений нет" << endl;
-            break;
-        default:
-            throw runtime_error("Тип аккаунта не позволяет совершить действия с его счётом!");
-            break;
-        };
-    }
-    catch (const runtime_error& e)
-    {
-        cerr << e.what() << endl;
-    }
-};
-float Account::getBalance() {
+float       Account::getBalance() {
     return this->balance;
 }; 
-bool Account::setBalance(float balance) {
-    try
-    {
-        this->balance = balance;
-        if (this->balance != balance)
-            throw runtime_error("Не удалось установить баланс");
-        
-        return true;
-    }
-    catch (const runtime_error& e)
-    {
-        cerr << e.what() << endl;
-        return false;
-    }
-   
+void        Account::setBalance(float balance) {
+    this->balance = balance;
 };
-float Account::getPercent() {
+float       Account::getPercent() {
     return this->percent;
 };
-bool Account::setPercent(float percent) {
-    try
-    {
-        this->percent = percent;
-        if (this->percent != percent)
-            throw runtime_error("Не удалось установить значение годовых процентов");
-    }
-    catch (const runtime_error& e)
-    {
-        cerr << e.what() << endl;
-    }
+void        Account::setPercent(float percent) {
+    this->percent = percent;
 }
+Account&    Account::operator=(const Account& r) {
+    this->type = r.type;
+    this->balance = r.balance;
+    this->percent = r.percent;
 
+    return *this;
+}
 
 
 // Определение работы класса User
@@ -117,75 +78,123 @@ User::User() {
 User::User(const char* name) {
     strcpy(all_name, name);
 };
-char* User::get_all_name() {
+char*       User::get_all_name() {
     return this->all_name;
 };
-int User::find_max_balance() {
-    try {
-        if (count_accounts <= 0)
-            throw runtime_error("Ещё не открыто ни одного счёта");
-        float max_balance = accounts[0].getBalance();
-        int index = 0;
-        for (int i = 1; i < count_accounts; i++)
-        {
-            if (max_balance >= accounts[i].getBalance())
-                continue;
-            else {
-                max_balance = accounts[i].getBalance();
-                index = i;
-            }
+int			User::get_count_accounts() {
+    return this->count_accounts;
+}
+int         User::find_max_balance() {
+    if (count_accounts <= 0)
+        throw runtime_error("Ещё не открыто ни одного счёта");
+    float max_balance = select_account_with_index(0).getBalance();
+    int index = 0;
+    for (int i = 1; i < count_accounts; i++)
+    {
+        if (max_balance >= select_account_with_index(i).getBalance())
+            continue;
+        else {
+            max_balance = select_account_with_index(i).getBalance();
+            index = i;
         }
-        return index;
-    } 
-    catch(const runtime_error& e) {
-        cerr << e.what() << endl;
-        return -1;
     }
-    
+    return index;
 };
-bool User::create_new_account(TypeScore type, float balance) {
-    try
-    {
-        if (type != TypeScore::calculated)
-            throw runtime_error("Ошибка программиста, для всех типов счёта, кроме рассчётного необходимо передать кол-во процентво годовых!");
-        if (balance < 0)
-            throw runtime_error("Нельзя создать не кредитный счёт с балансом меньше 0");
-        accounts[count_accounts] = Account(type);
-        accounts[count_accounts].setBalance(balance);
-
-        count_accounts += 1;
-        return true;
-    }
-    catch (const runtime_error& e)
-    {
-        cerr << e.what() << endl;
-        return false;
-    }
+Account&    User::create_calculated_account(unsigned int index, TypeScore type, float balance) {
+    if (type != TypeScore::calculated)
+        throw runtime_error("Ошибка программиста, для всех типов счёта, кроме рассчётного необходимо передать кол-во процентво годовых!");
+    if (balance < 0)
+        throw runtime_error("Нельзя создать не кредитный счёт с балансом меньше 0");
     
-}; 
+    Account new_el = select_account_with_index(index);
+    new_el = Account(type);    // через конструктор, потому что не могу обратиться к чужой приватной переменной
+    new_el.setBalance(balance);
+    new_el.setPercent(0);
 
-bool User::create_new_account(TypeScore type, float balance, float percent) {
-    try
-    {
-        if (type == TypeScore::calculated)
-            throw runtime_error("Ошибка программиста, для рассчётного счёта кол-во процентов годовых не нужно указывать!");
-        if (balance < 0 && type != TypeScore::credit)
-            throw runtime_error("Нельзя создать не кредитный счёт с балансом меньше 0");
-        if (percent < 0)
-            throw runtime_error("Нельзя создать счёт с кол-вом процентов годовых меньше 0");
-        accounts[count_accounts] = Account(type);
-        accounts[count_accounts].setBalance(balance);
-        accounts[count_accounts].setPercent(percent);
+    ++count_accounts;
+}
+Account&    User::create_dep_or_cre_account(unsigned int index, TypeScore type, float balance, float percent) {
+    if (type == TypeScore::calculated)
+        throw runtime_error("Ошибка программиста, для рассчётного счёта кол-во процентов годовых не нужно указывать!");
+    if (balance < 0 && type != TypeScore::credit)
+        throw runtime_error("Нельзя создать не кредитный счёт с балансом меньше 0");
+    if (percent < 0)
+        throw runtime_error("Нельзя создать счёт с кол-вом процентов годовых меньше 0");
 
-        count_accounts += 1;
-        return true;
-    }
-    catch (const runtime_error& e)
+    Account new_el = select_account_with_index(index);
+    new_el = Account(type);    // через конструктор, потому что не могу обратиться к чужой приватной переменной
+    new_el.setBalance(balance);
+    new_el.setPercent(percent);
+
+    ++count_accounts;
+}
+Account&    User::create_new_account_in_index(unsigned int index, TypeScore type, float balance) {
+    if (count_accounts >= MAX_COUNT_ACCOUNTS)
+        throw runtime_error("Максимальное кол-во аккаунтов уже создано");
+    for (size_t i = 0; i < MAX_COUNT_ACCOUNTS; i++)
     {
-        cerr << e.what() << endl;
-        return false;
+        if (index < count_accounts - i) {
+            Account left = select_account_with_index(count_accounts - i);
+            Account right = select_account_with_index(count_accounts - i + 1);
+            left = right;
+        }
+        else
+            return create_calculated_account(index,type,balance);
     }
-    
+}
+Account&    User::create_new_account_in_index(unsigned int index, TypeScore type, float balance, float percent) {
+    if (index >= MAX_COUNT_ACCOUNTS || index >= count_accounts)
+        throw runtime_error("Индекс, передаваемый в функцию, слишком большой");
+    if (count_accounts >= MAX_COUNT_ACCOUNTS)
+        throw runtime_error("Максимальное кол-во аккаунтов уже создано");
+    for (size_t i = 0; i < MAX_COUNT_ACCOUNTS; i++)
+    {
+        if (index < count_accounts - i) {
+            Account left = select_account_with_index(count_accounts - i);
+            Account right = select_account_with_index(count_accounts - i + 1);
+            left = right;
+        }
+        else
+            return create_dep_or_cre_account(index, type, balance, percent);
+    }
+}
+Account&    User::create_new_account(TypeScore type, float balance) {
+    if (count_accounts >= MAX_COUNT_ACCOUNTS)
+        throw runtime_error("Максимальное кол-во аккаунтов уже создано");
+    return create_calculated_account(count_accounts, type, balance);
 };
-
+Account&    User::create_new_account(TypeScore type, float balance, float percent) {
+    if (count_accounts >= MAX_COUNT_ACCOUNTS)
+        throw runtime_error("Максимальное кол-во аккаунтов уже создано");
+    return create_dep_or_cre_account(count_accounts, type, balance, percent);
+};
+Account&    User::select_account_with_index(unsigned int index) {
+    if (index >= MAX_COUNT_ACCOUNTS || index >= count_accounts)
+        throw runtime_error("Индекс, передаваемый в функцию, слишком большой");
+    return accounts[index];
+}
+Account     User::delete_account_with_index(unsigned int index) {
+    if (!count_accounts)
+        throw runtime_error("Нет ни одного аккаунта");
+    if (index >= MAX_COUNT_ACCOUNTS || index >= count_accounts)
+        throw runtime_error("Индекс, передаваемый в функцию, слишком большой");
+    
+    Account copy_account = accounts[index];
+    for (size_t i = index; i < MAX_COUNT_ACCOUNTS; i++)
+    {
+        if (i < count_accounts - 1) {
+            Account left_el = select_account_with_index(i);
+            Account right_el = select_account_with_index(i + 1);
+            left_el = right_el;
+        }
+        else
+        {
+            Account del_el = select_account_with_index(i);
+            del_el.type = TypeScore::none;
+            del_el.balance = 0;
+            del_el.percent = 0;
+        }
+    }
+    return copy_account;
+}
 
